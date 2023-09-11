fe34a4e502e7f1881189024db0ae1ac2d4c48df9
EvaCherkun <144095478+EvaCherkun@users.noreply.github.com>
2023-09-11 19:06:20 +0300

    Add files via upload
    ПЗ-1, Основи інформаційної безпеки, Черкун Єва

---
+++ 1.1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int seed1 = (int)DateTime.Now.Ticks; // властивість у класі DateTime, різне seed-значення для кожного запуску програми,яке визначатиметься за поточним часом.
+        int seed2 = seed1 + 1; // Змінюємо seed для другої послідовності
+
+
+        Random randomGenerator1 = new Random(123);
+        Random randomGenerator2 = new Random(seed2);
+
+        Console.WriteLine("Sequence 1:");
+
+        for (int i = 0; i < 15; i++) 
+        {
+            int randomNumber = randomGenerator1.Next(1, 10); 
+            Console.Write(randomNumber + " "); 
+        }
+
+        Console.WriteLine("\n\nSequence 2:");
+
+        for (int i = 0; i < 5; i++) 
+        {
+            int randomNumber = randomGenerator2.Next(1, 10); 
+            Console.Write(randomNumber + " "); // Виводимо числа горизонтально
+        }
+
+        Console.WriteLine(); 
+    }
+}
---
+++ 1.2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Cryptography;
+
+class RNGCryptoServiceProviderDemo
+{
+    static void Main()
+    {
+        Console.WriteLine("Generating two cryptographically stable sequences of random numbers");
+
+        // Генеруємо першу криптографічно стійку послідовність
+        var rndNumberGenerator1 = new RNGCryptoServiceProvider();
+        var randomNumber1 = new byte[64];
+        rndNumberGenerator1.GetBytes(randomNumber1);
+
+        // Генеруємо другу криптографічно стійку послідовність
+        var rndNumberGenerator2 = new RNGCryptoServiceProvider();
+        var randomNumber2 = new byte[64];
+        rndNumberGenerator2.GetBytes(randomNumber2);
+
+        Console.WriteLine("Random Sequence 1:");
+        Console.WriteLine(Convert.ToBase64String(randomNumber1));
+
+        Console.WriteLine("\nRandom Sequence 2:");
+        Console.WriteLine(Convert.ToBase64String(randomNumber2));
+    }
+}
